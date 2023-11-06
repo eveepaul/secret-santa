382edbb7d03298e9781412920ff2eeb92c1d2133
Yves Cabaneros <yves@MacBook-Pro.local>
2023-11-06 23:20:32 +0800

    add and remove input UI

--- components/PoolUserInput.vue
+++ components/PoolUserInput.vue
@@ -2,13 +2,15 @@
 <script lang="ts" setup>
 import { UserInput } from '~/types/userTypes';
 
-
-const props = defineProps<UserInput>();
-const emits = defineEmits(['update:email', 'update:name']);
+type Props = UserInput & {
+  id: number;
+};
+const props = defineProps<Props>();
+const emits = defineEmits(['update:email', 'update:name', 'removeInput']);
 const { email, name } = useVModels(props, emits);
 </script>
 <template>
-  <div class="px-4 pt-2 flex flex-col">
+  <div class="px-4 pt-2 flex flex-col ml-8">
     <div class="flex justify-between gap-4">
       <UFormGroup
         class="flex-grow"
@@ -27,6 +29,21 @@ const { email, name } = useVModels(props, emits);
           v-model="name"
         />
       </UFormGroup>
+      <div class="flex flex-col justify-center">
+        <UButton
+          v-if="props.id > 2"
+          :padded="false"
+          color="red"
+          variant="link"
+          class="items-end mt-5"
+          icon="i-heroicons-x-mark-20-solid"
+          @click="$emit('removeInput', { id })"
+        />
+        <div
+          v-else
+          class="ml-5"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,34 @@ definePageMeta({
     middleware: 'protected',
 });
 
-const usersInput = ref<UserInput[]>([{
+const initUsers = [{
     name: '',
     email: ''
-}]);
+}, {
+    name: '',
+    email: ''
+}, {
+    name: '',
+    email: ''
+}];
+const usersInput = ref<UserInput[]>(initUsers);
 
 const user = useUser();
 const poolIsOpen = ref(false);
 const openPool = () => {
     poolIsOpen.value = true;
 };
+
+const addInput = () => {
+    usersInput.value.push({
+        name: '',
+        email: ''
+    });
+};
+
+const removeInput = (data: { id: number; }) => {
+    usersInput.value.splice(data.id, 1);
+}
 
 </script>
 <template>
@@ -23,14 +41,45 @@ const openPool = () => {
             @open-pool="openPool"
         />
         <UModal v-model="poolIsOpen">
-            <div class="my-4">
+            <UCard>
+                <template #header>
+                    <Placeholder class="h-8" />
+                </template>
+
                 <PoolUserInput
                     v-for="(userInput, id) in usersInput"
                     :key="id"
+                    :id="id"
                     v-model:email="userInput.email"
                     v-model:name="userInput.name"
+                    @remove-input="removeInput"
                 />
-            </div>
+                <div class="flex justify-center mt-4">
+                    <UButton
+                        icon="i-heroicons-user-plus"
+                        size="sm"
+                        color="primary"
+                        variant="outline"
+                        label="Add more person"
+                        :trailing="false"
+                        @click="addInput"
+                    />
+                </div>
+
+                <template #footer>
+                    <div class="flex justify-end">
+
+                        <UButton
+                            size="sm"
+                            color="primary"
+                            variant="solid"
+                            label="Create"
+                            :trailing="false"
+                            class="items-end"
+                        />
+                    </div>
+                </template>
+            </UCard>
         </UModal>
     </div>
 </template>
